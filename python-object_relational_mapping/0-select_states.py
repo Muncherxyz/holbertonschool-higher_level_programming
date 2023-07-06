322cbe975030f06877bc1d25895aa148f52cfa41
Muncherxyz <19kortlanblocker@gmail.com>
2023-07-05 19:43:45 -0700

    fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-'''Get All States, requires User, Password, Database'''
+"""lists all states within hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
-
-def get_all_states():
-    '''Get States from sql table'''
-    if(len(sys.argv) < 4):
-        print(sys.argv)
-        return
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    try:
-        db = MySQLdb.connect(
-            "localhost",
-            mysql_username,
-            mysql_password,
-            database_name)
-    except Exception as e:
-        return (0)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for r in rows:
-        print(r)
-    cursor.close()
-    db.close()
-
-
 if __name__ == '__main__':
-    get_all_states()
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
