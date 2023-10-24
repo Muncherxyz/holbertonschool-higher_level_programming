f4393b02405c11d1c44829832ecf6001be9d3b00
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2023-10-24 16:16:57 -0500

    syntaxErrorFix

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creating user, setting pass,  granting user
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
