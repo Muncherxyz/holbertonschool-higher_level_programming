651862c17e31d8c0ec4587c461456c35a87d73b5
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2023-10-24 07:15:54 -0500

    Task9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- creating a 2nd table in database and also store data to tables
+CREATE TABLE IF NOT EXISTS second_table(
+id INT,
+name VARCHAR(256),
+score INT
+);
+
+INSERT INTO second_table(id, name, score)
+VALUES (1, "John", 10), (2, "Alex", 3), (3,"Bob", 14), (4, "George", 8)
