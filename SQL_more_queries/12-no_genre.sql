f8569b8d8e316cde245ff8d28ac2708356130c84
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2023-10-24 16:21:50 -0500

    Task12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- Show TV shows with NO genre data
+SELECT tv_shows.title, tv_show_genres.genre_id FROM (tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id) WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title ASC;
