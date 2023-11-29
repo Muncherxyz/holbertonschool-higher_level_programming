9acc50e9cac58a3a4ca4da4af01304a4d6c94052
Muncherxyz <101988752+Muncherxyz@users.noreply.github.com>
2023-11-28 20:28:18 -0600

    add created

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (!isNaN(a) && !isNaN(b)) {
+  add(a, b);
+} else {
+  console.log('NaN');
+}
